76411ccecb05bd79f291049e4ce157c64cb9705f
vickychurhe <churhevijay@gmail.com>
2024-01-05 15:36:38 +0530

    loop
    loop

---
+++ 06_for_Assignment.js
@@ -0,0 +1,29 @@
+
+console.log("-----WAP to print number from 5 to 15 by Incrementing 1-----");
+for (let index = 5; index <=15; index++) {
+    console.log(index);  
+}
+console.log("-----WAP to print number from 50 to 40 by Decrementing 1-----");
+for (let index = 50; index>=40; index= index-1) {
+    console.log(index); 
+}
+console.log("-----WAP to find first 15 odd number-----");
+for (let index = 1; index <=30; index=index+2) {
+    console.log(index); 
+}
+console.log("-----WAP to find first 10 even number-----");
+for (let index = 2; index <=20; index=index+2) {
+    console.log(index);    
+}
+console.log("-----WAP to print table of 5 to 50-----");
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);    
+}
+console.log("-----WAP to print table of 10 t0 100-----");
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);    
+}
+console.log("-----WAP to print table increasing order like 100 to 10-----");
+for (let index = 100; index >=10; index=index-10) {
+    console.log(index);    
+}
